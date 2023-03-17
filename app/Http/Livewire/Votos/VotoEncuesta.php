105bdeb371a19cd2eaf6311695d5a26d1daced13
Miguel-Uicab <56315474+Miguel-Uicab-MORT@users.noreply.github.com>
2023-03-17 14:18:36 -0600

    cambio de uso de las encuestas

--- app/Http/Livewire/Votos/VotoEncuesta.php
+++ app/Http/Livewire/Votos/VotoEncuesta.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire\Votos;
 
 use App\Models\Encuesta;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class VotoEncuesta extends Component
 {
+
     public function render()
     {
-        $encuestas = Encuesta::where('estado', Encuesta::Abierta)->get();
+        $user = User::find(Auth::id());
+
+        $encuestas = $user->encuesta()->get();
 
         return view('livewire.votos.voto-encuesta', compact('encuestas'));
     }
