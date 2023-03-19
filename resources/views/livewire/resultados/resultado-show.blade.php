bf8638c1ac7d86bcbbc0eac71fa4a0f5f4be5e8b
Miguel-Uicab <56315474+Miguel-Uicab-MORT@users.noreply.github.com>
2023-03-19 15:53:22 -0600

    Update resultado-show.blade.php

--- resources/views/livewire/resultados/resultado-show.blade.php
+++ resources/views/livewire/resultados/resultado-show.blade.php
@@ -7,9 +7,6 @@
                 </h2>
             </div>
             <div>
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ __('Total: '. $participante->respuestas->sum('calificacion'); }}
-                </h2>
             </div>
             <div>
                 <x-btn-print-ticket href="{{route('resultados.index')}}">
